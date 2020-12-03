7e051c7399be25d14b9b5165e612e7ff210e88b6
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-03 19:31:12 +0000

    mongo and strapi up and running

---
+++ backend/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c01aaa37c85fe1e35d46e20891428a2'),
+    },
+  },
+});
---
+++ backend/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '479d0ba7-2556-45a5-a940-83dcdc81d025'
+};
