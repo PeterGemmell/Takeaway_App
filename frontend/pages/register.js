ec698d7ad8dc276ee5e7e849039e10f1cde723c8
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-07 19:23:38 +0000

    register file complete

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -116,11 +116,37 @@ const Register = () => {
  </Row>
  <style jsx>
   {`
-    `}
-  )
-}
-
-
-export default () => {
-  return <h1>Sign Up</h1>;
+    .paper {
+      border: 1px solid lightgray;
+      box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+        0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+        0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+      border-radius: 6px;
+      margin-top: 90px;
+    }
+    .notification {
+      color: #ab003c;
+    }
+    .header {
+      width: 100%;
+      height: 120px;
+      background-color: #2196f3;
+      margin-bottom: 30px;
+      border-radius-top: 6px;
+    }
+    .wrapper {
+      padding: 10px 30px 20px 30px !important;
+    }
+    a {
+      color: blue !important;
+    }
+    img {
+      margin: 15px 30px 10px 50px;
+    }
+  `}
+  </style>
+  </Container>
+ );
 };
+
+export default Register;
