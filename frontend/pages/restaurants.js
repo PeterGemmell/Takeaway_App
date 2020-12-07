cfd9a012bf7e23f26a69f0194c0db25aedc796a5
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-07 13:26:04 +0000

    restaurants file completed

--- frontend/pages/restaurants.js
+++ frontend/pages/restaurants.js
@@ -36,4 +36,61 @@ function Restaurants(props) {
   const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, {
     variables: { id: router.query.id },
   });
+
+  if (error) return "Error Loading Dishes";
+  if (loading) return <h1>Loading ...</h1>;
+  if (data.restaurant) {
+    const { restaurant } = data;
+    return (
+      <>
+      <h1>{restaurant.name}</h1>
+      <Row>
+      {restaurant.dishes.map((res) => (
+        <Col xs="6" sm="4" style={{ padding: 0 }} key={res.id}>
+        <Card style={{ margin: "0 10px" }}>
+        <CardImg
+        top={true}
+        style={{ height: 250 }}
+        src={`${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
+        />
+        <CardBody>
+        <CardTitle>{res.name}</CardTitle>
+        <CardText>{res.description}</CardText>
+        </CardBody>
+        <div className="card-footer">
+        <Button outline color="primary">
+        + Add To Cart
+        </Button>
+
+        <style jsx>
+        {`
+          a {
+            color: white;
+          }
+          a:link {
+            text-decoration: none;
+            color: white;
+          }
+          .container-fluid {
+            margin-bottom: 30px;
+          }
+          .btn-outline-primary {
+            color: #007bff !important;
+          }
+          a:hover {
+            color: white !important;
+          }
+        `}
+        </style>
+        </div>
+        </Card>
+        </Col>
+      ))}
+    </Row>
+    </>
+   );
+  }
+  return <h1>Add Dishes</h1>;
 }
+
+export default Restaurants;
