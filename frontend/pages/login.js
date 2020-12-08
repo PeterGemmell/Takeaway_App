69c00261d5d228de0c6032d2fe18328cf71c17b6
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-08 11:16:25 +0000

    working on sign in page

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,4 +1,33 @@
 /* pages/login.js */
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
+import { login } from "../lib/auth";
+import AppContext from "../components/Context/AppContext";
+
+function Login(props) {
+  const [data, updateData] = useState({ identifier: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const router = useRouter();
+  const appContext = useContext(AppContext);
+}
+
+
+
+
+
+
+
 export default () => {
   return <h1> Sign In</h1>;
 };
