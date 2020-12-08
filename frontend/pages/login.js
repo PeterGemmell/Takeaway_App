1ea709fb984cb575ed0a06042ab83498806c7ca2
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-08 12:22:30 +0000

    sign in page complete

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -73,8 +73,75 @@ function Login(props) {
     style={{ height: 50, fontSize: "1.2em" }}
     />
     </FormGroup>
-  )
+
+    <FormGroup>
+    <span>
+    <a href="">
+    <small>Forgot Password?</small>
+    </a>
+    </span>
+    <Button
+    style={{ float: "right", width: 120 }}
+    color="primary"
+    onClick={() => {
+      setLoading(true);
+      login(data.identifier, data.password)
+      .then((res) => {
+        setLoading(false);
+        // set authed User in global context to update header/app state
+        appContext.setUser(res.data.user);
+      })
+      .catch((error) => {
+        setError(error.response.data);
+        setLoading(false);
+      });
+    }}
+   >
+    {loading ? "Loading... " : "Submit"}
+    </Button>
+    </FormGroup>
+    </fieldset>
+    </Form>
+    </section>
+    </div>
+    </Col>
+    </Row>
+    <style jsx>
+    {`
+      .paper {
+        border: 1px solid lightgray;
+        box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+            0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+            0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+        border-radius: 6px;
+        margin-top: 90px;
+      }
+      .notification {
+        color: #ab003c;
+      }
+      .header {
+        width: 100%;
+        height: 120px;
+        background-color: #2196f3;
+        margin-bottom: 30px;
+        border-radius-top: 6px;
+      }
+      .wrapper {
+        padding: 10px 30px 20px 30px !important;
+      }
+      a {
+        color: blue !important;
+      }
+      img {
+        margin: 15px 30px 10px 50px;
+      }
+    `}
+    </style>
+    </Container>
+  );
 }
+
+export default Login;
 
 
 
