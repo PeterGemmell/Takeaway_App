69d8efc3c4de4ca8cc201ad4202e769252ca24f8
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-08 11:33:08 +0000

    login form updated

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -20,6 +20,60 @@ function Login(props) {
   const [error, setError] = useState(false);
   const router = useRouter();
   const appContext = useContext(AppContext);
+
+  useEffect(() => {
+    if (appContext.isAuthenticated){
+      router.push("/"); // redirect if you're already logged in
+    }
+  }, []);
+
+  function onChange(event) {
+    updateData({ ...data, [event.target.name]: event.target.value });
+  }
+
+  return (
+    <Container>
+    <Row>
+    <Col sm="12" md={{ size: 5, offset: 3 }}>
+    <div className="paper">
+    <div className="header">
+    <img src="https://strapi.io/assets/images/logo.png" />
+    </div>
+    <section className="wrapper">
+    {Object.entries(error).length !== 0 &&
+    error.constructor === Object &&
+    error.message.map((error) => {
+      return (
+        <div
+        key={error.messages[0].id}
+        style={{ marginBottom: 10 }}
+        >
+        <small style={{ color: "red" }}>
+        {error.messages[0].message}
+        </small>
+        </div>
+      );
+    })}
+    <Form>
+    <fieldset disabled={loading}>
+    <FormGroup>
+    <Label>Email:</Label>
+    <Input
+    onChange={(event) => onChange(event)}
+    name="identifier"
+    style={{ height: 50, fontSize: "1.2em" }}
+    />
+    </FormGroup>
+    <FormGroup style={{ marginBottom: 30 }}>
+    <Label>Password:</Label>
+    <Input
+    onChange={(event) => onChange(event)}
+    type="password"
+    name="password"
+    style={{ height: 50, fontSize: "1.2em" }}
+    />
+    </FormGroup>
+  )
 }
 
 
