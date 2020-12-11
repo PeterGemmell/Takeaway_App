4876fa49e2b4c10dccce78b62b05e175ec068d8e
Peter Gemmell <petergemmell65@hotmail.com>
2020-12-11 12:07:37 +0000

    removed heroku setup

--- frontend/components/RestaurantList/index.js
+++ frontend/components/RestaurantList/index.js
@@ -46,9 +46,12 @@ function RestaurantList(props) {
                 <CardImg
                   top={true}
                   style={{ height: 250 }}
-                  src={ process.env.NODE_ENV === "production"
-                     ? res.image.url : `${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
-                     />
+                  src={
+                    process.env.NODE_ENV === "production"
+                      ? res.image[0].url
+                      : `${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`
+                  }
+                />
                 <CardBody>
                   <CardTitle>{res.name}</CardTitle>
                   <CardText>{res.description}</CardText>
